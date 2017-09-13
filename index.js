ff0e6643ec4c03b4f3f5787a161ad1f1fca6a20c
Joseph Vint <josephvint@me.com>
2017-09-13 00:21:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function yourAnimal() {
   var yourAnimal = 'cat'
 
   console.log(`${yourAnimal}`)
-  
+
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
